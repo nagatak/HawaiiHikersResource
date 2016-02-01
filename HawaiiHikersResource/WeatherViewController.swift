f72a30a6b46e49eb8cf51188d92c1243d9f9ae83
nagatak <nagatak@hawaii.edu>
2016-02-01 09:47:59 -1000

    Changed weather api from openweathermap to noaa

--- HawaiiHikersResource/WeatherViewController.swift
+++ HawaiiHikersResource/WeatherViewController.swift
@@ -21,21 +21,39 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var pressureLabel: UILabel!
     
     // Apikey
-    let openWeatherAppID = "c95098c4ab0d79ac8dafc441b786c5a4"
+    //let openWeatherAppID = "c95098c4ab0d79ac8dafc441b786c5a4"
     //Change "Hilo"
-    var city = "Hilo"
+    //var city = "Hilo"
+    
+    var lat : String!
+    var lon : String!
+    
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        lat = "19.692437"
+        lon = "-155.084503"
+        
+        
         //Gets weather data from openweathermap.org, returns a json file.
         //Change Hilo,usa to locationManager.Lat, locationManager.Lon for weather data at current location.
         //getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=Hilo,usa&appid=8ade0e3007865a732a6e6abec729fbd4")
-        getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=\(city),usa&appid=\(openWeatherAppID)&units=imperial")
+        //getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=\(city),usa&appid=\(openWeatherAppID)&units=imperial")
+        
+        getWeatherData("http://forecast.weather.gov/MapClick.php?lat=\(lat)&lon=\(lon)&FcstType=json")
+        
     }
     
     // Gets weather data
     func getWeatherData(urlString: String) {
+        
+        
+        //let weatherURL: NSURL = NSURL(string:"http://forecast.weather.gov/MapClick.php?lat="+lat+"&lon="+lon+"&FcstType=json")!
+        //let data = NSData(contentsOfURL: weatherURL)!
+        
         
         // FIXME: will crash if passed a nil is unwrapped.
         var url : NSURL = NSURL(string: urlString)!
@@ -44,8 +62,10 @@ class WeatherViewController: UIViewController {
             (data, response, error) -> Void in
             self.setLabels(data!)
         })
-        task.resume()
         
+        
+        task.resume()
+
     }
     
     // Sets data labels with retrieved weather information from JSON file
@@ -54,24 +74,44 @@ class WeatherViewController: UIViewController {
         var names = [String]()
         
         do {
-            //parse json into file
-            let json = try NSJSONSerialization.JSONObjectWithData(weatherData, options: .AllowFragments)
-            //checks extract info from json according to specified fields
-            if let name = json["name"] as? String {
-                locationLabel.text = name
-            }
-            if let main = json["main"] as? NSDictionary{
-                if let temp = main["temp"] as? Double {
-                    tempLabel.text = String(format: "%.1f", temp)
-                }
-                if let humidity = main["humidity"] as? Double {
-                    humidityLabel.text = String(format: "%.1f", humidity)
-                }
-                if let pressure = main["pressure"] as? Double {
-                    pressureLabel.text = String(format: "%.1f", pressure)
-                }
-            }
+
+            let json = try NSJSONSerialization.JSONObjectWithData(weatherData, options: NSJSONReadingOptions(rawValue: 0)) as? NSDictionary
             
+            
+            if let json = json{
+                if let currentObservations = json["currentobservation"] as? NSDictionary {
+                    if let Temp = currentObservations["Temp"] as? String{
+                        print(Temp)
+                    }
+                    if let Name = currentObservations["name"] as? String{
+                        print(Name)
+                    }
+                    if let Date = currentObservations["Date"] as? String{
+                        print(Date)
+                    }
+                    if let DewPoint = currentObservations["Dewp"] as? String{
+                        print(DewPoint)
+                    }
+                    if let Gust = currentObservations["Gust"] as? String{
+                        print(Gust)
+                    }
+                    if let Visibility = currentObservations["Visibiity"] as? String{
+                        print(Visibility)
+                    }
+                    if let Winds = currentObservations["Winds"] as? String{
+                        print(Winds)
+                    }
+                    if let Elevation = currentObservations["elev"] as? String{
+                        print(Elevation)
+                    }
+                }
+                if let currentForecast = json["data"] as? NSDictionary {
+                    if let forecast = currentForecast["text"] as? NSArray{
+                        print(forecast[0])
+                    }
+                }
+ 
+            }
             
         } catch {
             //Throw an error if not a json or no file
